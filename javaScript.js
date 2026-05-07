898259be33efeef73b170da11d36b982e77cf4a3
Vansformatics <ivansayfuddin8@gmail.com>
2026-05-07 21:04:07 +0700

    menambahkan kontainer untuk informasi tugas

--- javaScript.js
+++ javaScript.js
@@ -4,17 +4,28 @@ const btnTambah = document.getElementById("btnTambahTodo");
 const daftarTugas = document.getElementById("listTugas");
 
 btnTambah.addEventListener("click", function() {
+
     // Ambil nilai input
     const taskValue = inputTask.value;
     const dateValue = inputDate.value;
+
+    // Validasi input
     if (inputTask.value === "" || inputDate.value === "") {
         alert("Tugas dan Tanggal tidak boleh kosong!");
         return;
     }
 
+    // Elemen list baru untuk tugas
     const listBaru = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerHTML = inputValue.value;
+
+    // Kontainer untuk informasi tugas
+    const infoTugas = document.createElement("div");
+    infoTugas.style.flex = "1";
+    infoTugas.innerHTML = `
+        <strong class="nama-tugas">${taskValue}</strong> 
+        <br>
+        <small>${dateValue}</small> - <span class="status-teks">Progress</span>
+    `;
 
     listBaru.appendChild(span);
     daftarTugas.appendChild(listBaru);
